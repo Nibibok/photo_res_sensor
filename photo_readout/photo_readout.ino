7415896964e68ecf860152a53ed5e9480c7c4b4b
Amund Kiste <amund@kiste.no>
2025-06-24 13:29:22 +0200

    now with closests-two averaging voting

--- photo_readout/photo_readout.ino
+++ photo_readout/photo_readout.ino
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <Arduino.h>
+#include <float.h>
 
 constexpr uint32_t ledPin = 13;   // select the pin for the LED
 float ADCvoltage = 5.0;           // max voltage a pin reads is 5.
@@ -17,6 +18,8 @@ constexpr int AVG_SIZE = 100;
 uint32_t maxADC = (pow(2, 10) - 1) * AVG_SIZE;
 
 constexpr int outPin[REDUNDANCY][3] = {{0, 4, 8}, {1, 5, 9}, {2, 6, 10}}; // output pins for the sensors
+
+#define READOUT 0 // set to 1 to enable serial readout, 0 to disable
 
 void setup()
 {
@@ -78,10 +81,11 @@ void loop()
           zReadings[i] += analogRead(A5);
         }
       }
-      
+
       pinMode(outPin[i][0], INPUT);
       pinMode(outPin[i][1], INPUT);
       pinMode(outPin[i][2], INPUT);
+#if READOUT == 1
       Serial.print("Redundancy ");
       Serial.print(i);
       Serial.print("\tx: ");
@@ -90,16 +94,17 @@ void loop()
       Serial.print(yReadings[i]);
       Serial.print("\tz: ");
       Serial.print(zReadings[i]);
-      
-      float Vx = (float)xReadings[i]/maxADC * ADCvoltage;
-      float Vy = (float)yReadings[i]/maxADC * ADCvoltage;
-      float Vz = (float)zReadings[i]/maxADC * ADCvoltage;
-      xResistance[i] = Rconst*(ADCvoltage - Vx)/Vx;
-      yResistance[i] = Rconst*(ADCvoltage - Vy)/Vy;
-      zResistance[i] = Rconst*(ADCvoltage - Vz)/Vz;
+#endif
+      float Vx = (float)xReadings[i] / maxADC * ADCvoltage;
+      float Vy = (float)yReadings[i] / maxADC * ADCvoltage;
+      float Vz = (float)zReadings[i] / maxADC * ADCvoltage;
+      xResistance[i] = Rconst * (ADCvoltage - Vx) / Vx;
+      yResistance[i] = Rconst * (ADCvoltage - Vy) / Vy;
+      zResistance[i] = Rconst * (ADCvoltage - Vz) / Vz;
       xIntensity[i] = 1e6 / xResistance[i];
       yIntensity[i] = 1e6 / yResistance[i];
       zIntensity[i] = 1e6 / zResistance[i];
+#if READOUT == 1
       Serial.prnt("\tRx: ");
       Serial.print(xResistance[i]);
       Serial.print("\tRy: ");
@@ -112,7 +117,68 @@ void loop()
       Serial.print(yIntensity[i]);
       Serial.print("\tIz: ");
       Serial.println(zIntensity[i]);
+#endif
     }
+    float x_corrected;
+    float x_diff = FLT_MAX;
+    float y_corrected;
+    float y_diff = FLT_MAX;
+    float z_corrected;
+    float z_diff = FLT_MAX;
+    for (int i = 0; i < REDUNDANCY - 1; i++)
+    {
+      for (int j = i + 1; j < REDUNDANCY; j++)
+      {
+        float x_diff_temp = xIntensity[i]/xIntensity[j];
+        if (x_diff_temp < 1)
+        {
+          x_diff_temp = 1 / x_diff_temp;
+        }
+        if (x_diff_temp < x_diff)
+        {
+          x_diff = x_diff_temp;
+          x_corrected = (xIntensity[i] + xIntensity[j]) / 2;
+        }
+        float y_diff_temp = yIntensity[i]/yIntensity[j];
+        if (y_diff_temp < 1)
+        {
+          y_diff_temp = 1 / y_diff_temp; 
+        }
+        if (y_diff_temp < y_diff)
+        {
+          y_diff = y_diff_temp;
+          y_corrected = (yIntensity[i] + yIntensity[j]) / 2;
+        }
+        float z_diff_temp = zIntensity[i]/zIntensity[j];
+        if (z_diff_temp < 1)
+        {
+          z_diff_temp = 1 / z_diff_temp;
+        }
+        if (z_diff_temp < z_diff)
+        {
+          z_diff = z_diff_temp;
+          z_corrected = (zIntensity[i] + zIntensity[j]) / 2;
+        }
+      }
+#if READOUT == 1
+      Serial.print("x_diff: ");
+      Serial.print(x_diff);
+      Serial.print("\ty_diff: ");
+      Serial.print(y_diff);
+      Serial.print("\tz_diff: ");
+      Serial.println(z_diff);
+#endif
+      Serial.print("x_corrected: ");
+      Serial.print(x_corrected);
+      Serial.print("\ty_corrected: ");
+      Serial.print(y_corrected);
+      Serial.print("\tz_corrected: ");
+      Serial.println(z_corrected);
+    }
+    
+
+
+
     lastT = micros();
   }
 }
